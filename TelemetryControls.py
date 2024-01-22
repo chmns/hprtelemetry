80f1091c9cdcee835fa03ac3f0f407cbb54dc2b3
Marie-Flavie Loire <109023674+chmns@users.noreply.github.com>
2024-01-22 00:10:33 +0000

    improvement coordinate display

--- TelemetryControls.py
+++ TelemetryControls.py
@@ -75,6 +75,57 @@ class ReadOut(Frame):
         self.final = Label(self, text="", fg=self.color, bg=BG_COLOR, font=("Arial Bold", 18))
         self.final.grid(column = 0, row = 6, sticky=(N,S))
 
+class LocationGrid(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master, bg="green", height=110)
+
+        self.pre = LocationRow(self, "PreLaunch:")
+        self.pre.grid(row=0, column=0, sticky=(E,W))
+        self.pre.grid_propagate(True)
+
+        self.cur = LocationRow(self, "InFlight:")
+        self.cur.grid(row=1, column=0, sticky=(E,W))
+        self.cur.grid_propagate(True)
+
+        self.post = LocationRow(self, "PostFlight:")
+        self.post.grid(row=2, column=0, sticky=(E,W))
+        self.post.grid_propagate(True)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure((0, 1, 2), weight=0)
+
+
+class LocationRow(Frame):
+    def set_location(self, lat, lon, alt):
+        self.lat.config(text=str(lat))
+        self.lon.config(text=str(lon))
+        self.alt.config(text=f"{str(alt)}m")
+        pass
+
+    def __init__(self,
+                 master,
+                 name,
+                 font: str = "Courier 20 bold",
+                 fg:  str = "#FFFFFF",
+                 bg: str = "#0F0F0F"):
+        
+        Frame.__init__(self, master, bg=bg)
+
+        self.name = Label(self, text=name, bg=bg, fg=fg, font=font, width=14, justify="right")
+        self.name.grid(row=0, column=0, sticky=(N,S))
+
+        self.lat = Label(self, text=f"-", bg=bg, fg=fg, font=font)
+        self.lat.grid(row=0, column=1, sticky=(N,S))
+        
+        self.lon = Label(self, text=f"-", bg=bg, fg=fg, font=font)
+        self.lon.grid(row=0, column=2, sticky=(N,S))
+        
+        self.alt = Label(self, text=f"0m", bg=bg, fg=fg, font=font)
+        self.alt.grid(row=0, column=3, sticky=(N,S))
+        
+        self.columnconfigure(0, weight=0)
+        self.columnconfigure((1,2,3), weight=1)
+
 class TiltAndSpin(Frame):
     SPACING = 30
 
