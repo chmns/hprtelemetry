4aa7c555e7ce01ef7682f361464e7479e000e8c0
Marie-Flavie Loire <109023674+chmns@users.noreply.github.com>
2024-03-02 12:37:00 +0000

    map frame working with flight telemetry

--- MapFrame.py
+++ MapFrame.py
@@ -1,25 +1,49 @@
 from tkinter import *
 import tkintermapview
 
+DEFAULT_LAT = 44.7916443
+DEFAULT_LON = -0.5995578
+DEFAULT_ZOOM = 14
+
+
 class MapFrame(Frame):
-    def __init__(self, master):
+
+    def update(self):
+        new_lat = self.lat_var.get()
+        new_lon = self.lon_var.get()
+        new_alt = self.alt_var.get()
+
+        if new_lat != self.lat or new_lon != self.lon:
+            self.lat = new_lat
+            self.lon = new_lon
+            self.map_view.set_position(self.lat, self.lon, marker=True)
+            self.location_grid.current.set_location(self.lat, self.lon, new_alt)
+
+    def reset(self):
+        self.lat = DEFAULT_LAT
+        self.lon = DEFAULT_LON
+        self.map_view.set_position(self.lat, self.lon)
+        self.map_view.set_zoom(DEFAULT_ZOOM)
+
+    def __init__(self, master, lat_var_name, lon_var_name, alt_var_name):
         Frame.__init__(self, master, bg="blue")
+
+        self.lat_var = DoubleVar(master, 0.0, lat_var_name)
+        self.lon_var = DoubleVar(master, 0.0, lon_var_name)
+        self.alt_var = DoubleVar(master, 0.0, alt_var_name)
 
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=0)
 
-        map = tkintermapview.TkinterMapView(self)
-        map.grid(row=0, column=0, sticky=(N,E,S,W))
-        map.lat = 44.7916443
-        map.lon = -0.5995578
-        map.set_position(map.lat, map.lon)
-        map.set_zoom(14)
-        map.grid_propagate(True)
+        self.map_view = tkintermapview.TkinterMapView(self)
+        self.map_view.grid(row=0, column=0, sticky=(N,E,S,W))
+        self.map_view.grid_propagate(True)
 
-        location_grid = LocationGrid(self)
-        location_grid.grid(row=1, column=0, sticky=(N,E,S,W))
-        location_grid.grid_propagate(False)
+        self.location_grid = LocationGrid(self)
+        self.location_grid.grid(row=1, column=0, sticky=(N,E,S,W))
+        self.location_grid.grid_propagate(False)
+        self.reset()
 
 class LocationGrid(Frame):
     def __init__(self, master):
@@ -29,9 +53,9 @@ class LocationGrid(Frame):
         self.pre.grid(row=0, column=0, sticky=(E,W))
         self.pre.grid_propagate(True)
 
-        self.cur = LocationRow(self, "InFlight:")
-        self.cur.grid(row=1, column=0, sticky=(E,W))
-        self.cur.grid_propagate(True)
+        self.current = LocationRow(self, "InFlight:")
+        self.current.grid(row=1, column=0, sticky=(E,W))
+        self.current.grid_propagate(True)
 
         self.post = LocationRow(self, "PostFlight:")
         self.post.grid(row=2, column=0, sticky=(E,W))
